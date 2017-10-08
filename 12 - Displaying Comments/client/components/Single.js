7a2738a9fd97ecc4c4d5a54d3a3b1c99bb3bf1e8
ijlee2 <ijlee2@users.noreply.github.com>
2017-10-08 18:06:14 -0500

    day 12 finished

--- 12 - Displaying Comments/client/components/Single.js
+++ 12 - Displaying Comments/client/components/Single.js
@@ -19,7 +19,7 @@ const Single = React.createClass({
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props} />
-                <Comments postComments={postComments} />
+                <Comments postComments={postComments} {...this.props} />
             </div>
         );
     }
